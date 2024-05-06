45080f72f4bf69e9aa55ef2a8602701c5182044f
Bruno Marazzi <brunomarazzidev@gmail.com>
2024-05-06 16:18:07 -0300

    up

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ btn.addEventListener("click", function () {
     btn.textContent = "Share a fact";
   }
 });
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return `Impossible year. Year needs to be less or equal ${currentYear}`;
+}
+
+console.log(calcFactAge(1994));
