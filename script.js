bd33e63f547d06991f9be9dd8c4becc3b06a0995
Bruno Marazzi <brunomarazzidev@gmail.com>
2024-05-09 15:47:31 -0300

    up

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+// Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factList = document.querySelector(".fact-list");
 
+// Create DOM elements: Render facts in list
+factList.innerHTML = "";
+
+// Toggle form visibility
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
@@ -11,19 +51,19 @@ btn.addEventListener("click", function () {
   }
 });
 
-function calcFactAge(year) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - year;
+// function calcFactAge(year) {
+//   const currentYear = new Date().getFullYear();
+//   const age = currentYear - year;
 
-  if (age >= 0) return age;
-  else return `Impossible year. Year needs to be less or equal ${currentYear}`;
-}
+//   if (age >= 0) return age;
+//   else return `Impossible year. Year needs to be less or equal ${currentYear}`;
+// }
 
-console.log(calcFactAge(1994));
+// console.log(calcFactAge(1994));
 
-const calcFactAge2 = (year) =>
-  year <= new Date().getFullYear()
-    ? new Date().getFullYear() - year
-    : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
+// const calcFactAge2 = (year) =>
+//   year <= new Date().getFullYear()
+//     ? new Date().getFullYear() - year
+//     : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
 
-console.log(calcFactAge2(1994));
+// console.log(calcFactAge2(1994));
