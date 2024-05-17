b4128fd6f5ec1ad269fe8533d5f1f9e168a21855
Bruno Marazzi <brunomarazzidev@gmail.com>
2024-05-17 16:44:01 -0300

    u

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import "./style.css";
 
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="Logo" />
-        <h1>Today I Learned</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+    <>
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Logo" />
+          <h1>Today I Learned</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <CategoryFilter />
+    </>
   );
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
 }
 
 export default App;
