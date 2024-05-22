495a6241abe5c5e50e9878a0d0fd4fc07910d922
Bruno Marazzi <brunomarazzidev@gmail.com>
2024-05-22 11:41:53 -0300

    up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./style.css";
 function App() {
   return (
     <>
+      {/* HEADER */}
       <header className="header">
         <div className="logo">
           <img src="logo.png" alt="Logo" />
@@ -10,14 +11,25 @@ function App() {
         </div>
         <button className="btn btn-large btn-open">Share a fact</button>
       </header>
-
-      <CategoryFilter />
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
     </>
   );
 }
 
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
 function CategoryFilter() {
   return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts list</section>;
 }
 
 export default App;
